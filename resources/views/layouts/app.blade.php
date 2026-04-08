cfbe72a4088c18bc8e7ff26d14bb36fedb6799b6
Rizky Dhani <rizkydhni03@gmail.com>
2026-04-08 15:32:54 +0700

    feat: add Flux config

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,14 +5,18 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <title>{{ $title ?? config('app.name') }}</title>
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=inter:400,500,600&display=swap" rel="stylesheet" />
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         @livewireStyles
+        @fluxAppearance
     </head>
     <body>
         {{ $slot }}
 
         @livewireScripts
+        @fluxScripts
     </body>
 </html>
